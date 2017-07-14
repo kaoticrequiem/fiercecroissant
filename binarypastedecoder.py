7b4ae25d2fb7080254ce9910f163d63b899dab47
Patrick Colford <kaoticrequiem@gmail.com>
2017-07-14 14:55:54 -0700

    Changed decoder to save pastes without extension

--- binarypastedecoder.py
+++ binarypastedecoder.py
@@ -1,7 +1,8 @@
 import binascii, os, requests
 
 base64list = []
-save_path = os.getcwd() + '/decodedexes/'
+save_path = '/home/ubuntu/patrick/decodedexes/'
+length = 8
 
 def writefile(filenm, stuff):
    writefile = open(filenm,'w')
@@ -11,18 +12,22 @@ def writefile(filenm, stuff):
 for filename in os.listdir("/home/ubuntu/patrick/pastes/binarypastes"):
    #request the raw text
    raw_text_url = 'http://pastebin.com/raw/'
-   outputfile = save_path + filename + ' decoded.exe'
+   outputfile = save_path + filename
    paste_data = requests.get(raw_text_url + filename).text
    response = requests.get(raw_text_url + filename)
    if response.status_code == 404:
        continue
    #decode it
-   os.rename("/home/ubuntu/patrick/pastes/binarypastes/" + filename, "/home/ubuntu/patrick/pastes/binarypastes/" + filename + " PROCESSED")
+   #os.rename("/home/ubuntu/patrick/pastes/binarypastes/" + filename, "/home/ubuntu/patrick/pastes/binarypastes/" + filename + " PROCESSED")
    if paste_data.isnumeric():
-      try:
-         decoded_paste = binascii.b2a_base64(paste_data)
-         writefile(outputfile, decoded_paste)
-      except:
-         print("I failed trying to decode paste ") + filename
-         raise
-         continue
+       try:
+          paste_data_length = [paste_data[i:i+length] for i in range(0,len(paste_data),length)]
+          decoded_paste = ''.join([chr(int(c,base=2)) for c in paste_data_length])
+          writefile(outputfile, decoded_paste)
+       except:
+          print("I failed trying to decode paste ") + filename
+          raise
+          continue
+
+    
+        
