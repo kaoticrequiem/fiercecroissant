fa812808dd6e8906054ab55d3f7703c46a180d9e
Patrick Colford <kaoticrequiem@gmail.com>
2017-08-07 15:40:09 -0700

    Change decoders to run locally.

--- base64pastedecoder.py
+++ base64pastedecoder.py
@@ -1,30 +1,22 @@
 import base64, os, requests
 
 base64list = []
-save_path = os.getcwd() + '/decodedexes/'
+decoded_save_path = '/home/ubuntu/patrick/decodedexes/'
 
 def writefile(filenm, stuff):
    writefile = open(filenm,'w')
    writefile.write(stuff)
    writefile.close()
 
-for filename in os.listdir("/home/ubuntu/patrick/pastes/base64pastes"):
-   #request the raw text
-   raw_text_url = 'http://pastebin.com/raw/'
-   outputfile = save_path + filename + ' decoded.exe'
-   paste_data = requests.get(raw_text_url + filename).text
-   response = requests.get(raw_text_url + filename)
-   if response.status_code == 404:
-       continue
-   #decode it
-   os.rename("/home/ubuntu/patrick/pastes/base64pastes/" + filename, "/home/ubuntu/patrick/pastes/base64pastes/" + filename + " PROCESSED")
-   missing_padding = len(paste_data) % 4
-   if missing_padding != 0:
-       paste_data += b'='* (4 - missing_padding)
-   decoded_paste = base64.b64decode(paste_data)
-# try:
-  # 	decoded_paste = base64.b64decode(paste_data)
-  # except Exception:
-  # 	pass
-   writefile(outputfile, decoded_paste)
-   #throw that into TG
+for filename in os.listdir('/home/ubuntu/patrick/pastes/base64pastes/'): # absolute path of saved base64 sorted pastes
+    sorted_path = os.path.join('/home/ubuntu/patrick/pastes/base64pastes/', filename)
+    outputfile = decoded_save_path + filename
+    with open(sorted_path, 'r') as f:
+        paste_data = f.read()
+        missing_padding = len(paste_data) % 4
+        if missing_padding != 0:
+            paste_data += b'='* (4 - missing_padding) # fix padding error
+        decoded_paste = base64.b64decode(paste_data)
+        writefile(outputfile, decoded_paste) # write pe32exe
+        os.remove(sorted_path)
+        f.close()
--- binarypastedecoder.py
+++ binarypastedecoder.py
@@ -4,30 +4,22 @@ base64list = []
 save_path = '/home/ubuntu/patrick/decodedexes/'
 length = 8
 
-def writefile(filenm, stuff):
-   writefile = open(filenm,'w')
-   writefile.write(stuff)
-   writefile.close()
+def writefile(path, binary):
+    writefile = open(path,'wb')
+    writefile.write(binary)
+    writefile.close()
 
-for filename in os.listdir("/home/ubuntu/patrick/pastes/binarypastes"):
-   #request the raw text
-   raw_text_url = 'http://pastebin.com/raw/'
-   outputfile = save_path + filename
-   paste_data = requests.get(raw_text_url + filename).text
-   response = requests.get(raw_text_url + filename)
-   if response.status_code == 404:
-       continue
-   #decode it
-   #os.rename("/home/ubuntu/patrick/pastes/binarypastes/" + filename, "/home/ubuntu/patrick/pastes/binarypastes/" + filename + " PROCESSED")
-   if paste_data.isnumeric():
-       try:
-          paste_data_length = [paste_data[i:i+length] for i in range(0,len(paste_data),length)]
-          decoded_paste = ''.join([chr(int(c,base=2)) for c in paste_data_length])
-          writefile(outputfile, decoded_paste)
-       except:
-          print("I failed trying to decode paste ") + filename
-          raise
-          continue
+for filename in os.listdir('/home/ubuntu/patrick/pastes/binarypastes/'):
+    sorted_path = os.path.join('/home/ubuntu/patrick/pastes/binarypastes/', filename)
+    length = 8
+    outputfile = save_path + filename
+    with open(sorted_path, 'r') as f:
+        paste_data = f.read()
+        paste_data_length = [paste_data[i:i+length] for i in range(0,len(paste_data),length)]
+        decoded_paste = ''.join([chr(int(c,base=2)) for c in paste_data_length])
+        writefile(outputfile, decoded_paste)
+        os.remove(sorted_path)
+        f.close()
 
     
         
