be0545c5cbcc6e212f98ddc6d85974e4c79e283f
Patrick Colford <kaoticrequiem@gmail.com>
2017-08-26 23:58:26 +0000

    Reverting changes.

--- fiercecroissant.py
+++ fiercecroissant.py
@@ -57,13 +57,12 @@ def trendscraper():
             }
             data_json = {'message': '<b>Joshtest - trendscraper part: New Paste<b>', 'card': card, 'message_format': 'html'}
             params = {'auth_token': hip_token}
-            r = requests.post('https://api.hipchat.com/v2/room/' + hip_room + '/notification', data=json.dumps(data_json), headers=headers, params=params)
+            r = requests.post('https://api.hipchat.com/v2/room/3370440/notification', data=json.dumps(data_json), headers=headers, params=params)
 
 def scrapebin():
     result_limit = 50
     sleep_time = 30  # interval in seconds to sleep after each recent pastes batch
     minimum_length = 10  # ignore pastes shorter than this
-    
     def http_get(url, params=None, tries=0):
         if params is None:
             params = {}
@@ -74,7 +73,6 @@ def scrapebin():
             return res
         time.sleep(1)
         return http_get(url, params, tries + 1)
-    
     def save_paste(path, data, separator=None):
         #if separator is None:
         #    separator = '\n\n---------- PASTE START ----------\n\n'
@@ -82,7 +80,6 @@ def scrapebin():
         #    file.write(json.dumps(paste, sort_keys=True, indent=3, separators=(',', ': ')) + separator)
             file.write(data)
         return file.closed
-    
     while True:
         clock = int(time.strftime('%M', time.localtime()))
         if clock == 5:
@@ -97,89 +94,64 @@ def scrapebin():
             paste_size = paste['size']
             print('\rScraping: {0} / {1}'.format(i + 1, result_limit))
             filename = save_path + paste['key']
+            pastekey = paste['key']
             #coll_pastemetadata.insert_one(paste)
 
             hexmatch = re.search(r'(\\x\w\w){100,}', paste_data) #Regex for hex formatted as "\\xDC", "\\x02", "\\xC4"
-            stringmatch = re.search(r'(A){20}', paste_data) #Searching for 10 'A's in a row.
-            base64match = re.search(r'\w{200,}', paste_data) #Searching for 200 characters in a row to get non-words.
-            base64sort = re.search(r'\A(TV(oA|pB|pQ|qQ|qA|ro|pA))', paste_data) #Searches the start of the paste for Base64 encoding structure.
-            binarymatch = re.search(r'(0|1){200,}', paste_data) #Searches for 200 0's or 1's in a row.
-            base64reversesort = re.search(r'\Z(AAAMAAQqVT)', paste_data) #Searches the end of the paste for reversed Base64 encoding structure.
-            hexmatch2 = re.search(r'[2-9A-F]{200,}', paste_data) #Regex for Hexadecimal encoding.
-            phpmatch = re.search(r'\A(<\?php)', paste_data) #Searches the start of a paste for php structure.
-            imgmatch = re.search(r'\A(data:image)', paste_data) #Searches the start of a paste for data:image structure.
+            stringmatch = re.search(r'(A){20}', paste_data) #Regex for 10 'A's in a row.
+            base64match = re.search(r'\w{200,}', paste_data) #Regex for Base64
+            base64sort = re.search(r'\A(TV(oA|pB|pQ|qQ|qA|ro|pA))', paste_data) #Sorting for base64.
+            binarymatch = re.search(r'(0|1){200,}', paste_data) #Regex for binary.
+            base64reversesort = re.search(r'\Z(AAAMAAQqVT)', paste_data) #Sorting for reversed base64
+            hexmatch2 = re.search(r'[2-9A-F]{200,}', paste_data) #Regex for Hex.
+            phpmatch = re.search(r'\A(<\?php)', paste_data)
+            imgmatch = re.search(r'\A(data:image)', paste_data)
             if os.path.isfile(filename) or int(paste['size']) < minimum_length:
                 continue
-            pastemetadata_dict = {'date': [], 'key': [], 'size': [], 'expire': [], 'syntax': [], 'user':[], 'encodingtype':[]}
-            if (hexmatch or stringmatch or base64match or base64sort or binarymatch or base64reversesort or phpmatch or imgmatch) is None:
-                #paste_data_dict['nomatch'].append(paste_data)                
+            #paste_data_dict = {'nomatch': [], 'pastekey': []}
+#            if (hexmatch or stringmatch or base64match or base64sort or binarymatch or base64reversesort or phpmatch or imgmatch) is None:
+                #paste_data_dict['nomatch'].append(paste_data)
                 #paste_data_dict['pastekey'].append(pastekey)
                 #coll_pasterawunsorted.insert_one(paste_data_dict)
-                if ((base64match or stringmatch) and int(paste_size) > 40000) and paste_lang == "text":
-                    filename = save_path + paste['key']
-                    if (binarymatch and paste_data.isnumeric()):
-                        filename = save_path_binary + paste['key']
-                        encodingtype = 'binary'
-                        save_paste(filename, paste_data)
-                        try:
-                            coll_pastemetadata.insert_one(paste)
-                        except:
-                            continue
-                    elif (base64sort or base64reversesort):
-                        filename = save_path_base64 + paste['key']
-                        encodingtype = 'base64'
-                        save_paste(filename, paste_data)
-                        try:
-                            coll_pastemetadata.insert_one(paste)
-                        except:
-                            continue
-                    elif (hexmatch or hexmatch2):
-                        filename = save_path_hex + paste['key']
-                        encodingtype = 'hexadecimal'
-                        save_paste(filename, paste_data)
-                        try:
-                            coll_pastemetadata.insert_one(paste)
-                        except:
-                            continue
-                    elif phpmatch:
-                        filename = save_path_php + paste['key']
-                        encodingtype = 'php'
-                        save_paste(filename, paste_data)
-                        try:
-                            coll_pastemetadata.insert_one(paste)
-                        except:
-                            continue
-                    elif imgmatch:
-                        filename = save_path_img + paste['key']
-                        encodingtype = 'img'
-                        save_paste(filename, paste_data)
-                        try:
-                            coll_pastemetadata.insert_one(paste)
-                        except:
-                            continue
-                    hits += 1
-                    headers = {'Content-Type': 'application/json'}
-                    card = {
-                        "style": "link",
-                        "url": "https://pastebin.com/" + paste['key'],
-                        "id": "fee4d9a3-685d-4cbd-abaa-c8850d9b1960",
-                        "title": "Pastebin Hit",
-                        "description": {
-                            "format": "html",
-                            "value": "<b>TEST: New Paste Seen:</b> <a href='https://pastebin.com/'" + paste['key'] + " data-target='hip-connect-tester:hctester.dialog.simple' data-target-options='{\"options\":{\"title\":\"Custom Title\"}, \"parameters\":{\"from\":\"link\"}}'>https://pastebin.com/" + paste['key'] + "</a>"
-                        },
-                        "icon": {
-                            "url": "https://pastebin.com/favicon.ico"
-                        },
-                        "date": 1443057955792
-                    }
-                    data_json = {'message': '<b>New Paste<b>', 'card': card, 'message_format': 'html'}
-                    params = {'auth_token': hip_token}
-                    r = requests.post('https://api.hipchat.com/v2/room/' + hip_room + '/notification', data=json.dumps(data_json),headers=headers, params=params)
-            print("\nHits: {0}".format(hits))
-            print("Waiting...\n\n")
-            time.sleep(sleep_time)
-
+            if ((base64match or stringmatch) and int(paste_size) > 40000) and paste_lang == "text":
+                filename = save_path + paste['key']
+                if (binarymatch and paste_data.isnumeric()):
+                    filename = save_path_binary + paste['key']
+                    save_paste(filename, paste_data)
+                elif (base64sort or base64reversesort):
+                    filename = save_path_base64 + paste['key']
+                    save_paste(filename, paste_data)
+                elif (hexmatch or hexmatch2):
+                    filename = save_path_hex + paste['key']
+                    save_paste(filename, paste_data)
+                elif phpmatch:
+                    filename = save_path_php + paste['key']
+                    save_paste(filename, paste_data)
+                elif imgmatch:
+                    filename = save_path_img + paste['key']
+                    save_paste(filename, paste_data)
+                hits += 1
+                headers = {'Content-Type': 'application/json'}
+                card = {
+                    "style": "link",
+                    "url": "https://pastebin.com/" + paste['key'],
+                    "id": "fee4d9a3-685d-4cbd-abaa-c8850d9b1960",
+                    "title": "Pastebin Hit",
+                    "description": {
+                        "format": "html",
+                        "value": "<b>TEST: New Paste Seen:</b> <a href='https://pastebin.com/'" + paste['key'] + " data-target='hip-connect-tester:hctester.dialog.simple' data-target-options='{\"options\":{\"title\":\"Custom Title\"}, \"parameters\":{\"from\":\"link\"}}'>https://pastebin.com/" + paste['key'] + "</a>"
+                    },
+                    "icon": {
+                        "url": "https://pastebin.com/favicon.ico"
+                    },
+                    "date": 1443057955792
+                }
+                data_json = {'message': '<b>New Paste<b>', 'card': card, 'message_format': 'html'}
+                params = {'auth_token': hip_token}
+                r = requests.post('https://api.hipchat.com/v2/room/' + hip_room + '/notification', data=json.dumps(data_json),headers=headers, params=params)
+        print("\nHits: {0}".format(hits))
+        print("Waiting...\n\n")
+        time.sleep(sleep_time)
 if __name__ == "__main__":
     while True:
         scrapebin()
