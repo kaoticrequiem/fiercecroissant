fb78e17749859bd46796daf85b78d512bb386f3f
Patrick Colford <kaoticrequiem@gmail.com>
2017-08-30 13:35:23 -0700

    Merged conflicts.

--- fiercecroissant.py
+++ fiercecroissant.py
@@ -60,123 +60,127 @@ def trendscraper():
             params = {'auth_token': hip_token}
             r = requests.post('https://api.hipchat.com/v2/room/' + hip_room + '/notification', data=json.dumps(data_json),headers=headers, params=params)
 
+
+
+def http_get(url, params=None, tries=0):
+    if params is None:
+        params = {}
+    if tries > 10:
+        sys.exit('Exceeded 10 fetch retries. Are you banned?')
+    res = requests.get(url, params, timeout=(4, 5))
+    if res.status_code == requests.codes.ok:
+        return res
+    time.sleep(1)
+    return http_get(url, params, tries + 1)
+
+def save_paste(path, data, separator=None):
+    #if separator is None:
+    #    separator = '\n\n---------- PASTE START ----------\n\n'
+    with open(path, 'w', encoding='utf-8') as file:
+    #    file.write(json.dumps(paste, sort_keys=True, indent=3, separators=(',', ': ')) + separator)
+        file.write(data)
+    return file.closed
+
+def metadatasave(paste, encodingtype):
+    pastemetadata_dict = {'date': [], 'key': [], 'size': [], 'expire': [], 'syntax': [], 'user':[], 'encodingtype':[]}
+    pastemetadata_dict.update({'date':paste['date'], 'key':paste['key'], 'size':paste['size'], 'expire':paste['expire'], 'syntax':paste['syntax'], 'user':paste['user'], 'encodingtype':encodingtype})
+    return pastemetadata_dict
+
+def hipchatpost():
+    headers = {'Content-Type': 'application/json'}
+    card = {
+        "style": "link",
+        "url": paste_url,
+        "id": "fee4d9a3-685d-4cbd-abaa-c8850d9b1960",
+        "title": "Pastebin Hit",
+        "description": {
+            "format": "html",
+            "value": "<b>TEST: New Paste Seen:</b>" + paste_url + " Encoded as:" + encodingtype
+                },
+    "icon": {
+        "url": "https://pastebin.com/favicon.ico"
+                },
+    "date": 1443057955792
+    }
+    data_json = {'message': '<b>New Paste<b>', 'card': card, 'message_format': 'html'}
+    params = {'auth_token': hip_token}
+    r = requests.post('https://api.hipchat.com/v2/room/' + hip_room + '/notification', data=json.dumps(data_json),headers=headers, params=params)
+
 def scrapebin():
     result_limit = 50
     sleep_time = 30  # interval in seconds to sleep after each recent pastes batch
     minimum_length = 10  # ignore pastes shorter than this
-    def http_get(url, params=None, tries=0):
-        if params is None:
-            params = {}
-        if tries > 10:
-            sys.exit('Exceeded 10 fetch retries. Are you banned?')
-        res = requests.get(url, params, timeout=(4, 5))
-        if res.status_code == requests.codes.ok:
-            return res
-        time.sleep(1)
-        return http_get(url, params, tries + 1)
-    def save_paste(path, data, separator=None):
-        #if separator is None:
-        #    separator = '\n\n---------- PASTE START ----------\n\n'
-        with open(path, 'w', encoding='utf-8') as file:
-        #    file.write(json.dumps(paste, sort_keys=True, indent=3, separators=(',', ': ')) + separator)
-            file.write(data)
-        return file.closed
 
-    def metadatasave(paste, encodingtype):
-        pastemetadata_dict = {'date': [], 'key': [], 'size': [], 'expire': [], 'syntax': [], 'user':[], 'encodingtype':[]}
-        pastemetadata_dict.update({'date':paste['date'], 'key':paste['key'], 'size':paste['size'], 'expire':paste['expire'], 'syntax':paste['syntax'], 'user':paste['user'], 'encodingtype':encodingtype})
-        return pastemetadata_dict
-
-    def hipchatpost():
-        headers = {'Content-Type': 'application/json'}
-        card = {
-            "style": "link",
-            "url": paste_url,
-            "id": "fee4d9a3-685d-4cbd-abaa-c8850d9b1960",
-            "title": "Pastebin Hit",
-            "description": {
-                "format": "html",
-                "value": "<b>TEST: New Paste Seen:</b>" + paste_url + " Encoded as:" + encodingtype
-                    },
-        "icon": {
-            "url": "https://pastebin.com/favicon.ico"
-                    },
-        "date": 1443057955792
-        }
-        data_json = {'message': '<b>New Paste<b>', 'card': card, 'message_format': 'html'}
-        params = {'auth_token': hip_token}
-        r = requests.post('https://api.hipchat.com/v2/room/' + hip_room + '/notification', data=json.dumps(data_json),headers=headers, params=params)
-    
-while True:
-    clock = int(time.strftime('%M', time.localtime()))
-    if clock == 5:
-        trendscraper()
-    else:
-        print("Waiting for new trends.")
-    hits = 0
-    recent_items = http_get('http://pastebin.com/api_scraping.php', params={'limit': result_limit}).json()
-    for i, paste in enumerate(recent_items):
-        paste_data = http_get(paste['scrape_url']).text
-        paste_lang = paste['syntax']
-        paste_size = paste['size']
-        paste_url = paste['full_url']
-        print('\rScraping: {0} / {1}'.format(i + 1, result_limit))
-        hexmatch = re.search(r'(\\x\w\w){100,}', paste_data) #Regex for hex formatted as "\\xDC", "\\x02", "\\xC4"
-        stringmatch = re.search(r'(A){20}', paste_data) #Searching for 10 'A's in a row.
-        base64match = re.search(r'\w{200,}', paste_data) #Searching for 200 characters in a row to get non-words.
-        base64sort = re.search(r'\A(TV(oA|pB|pQ|qQ|qA|ro|pA))', paste_data) #Searches the start of the paste for Base64 encoding structure.
-        binarymatch = re.search(r'(0|1){200,}', paste_data) #Searches for 200 0's or 1's in a row.
-        base64reversesort = re.search(r'\Z(AAAMAAQqVT)', paste_data) #Searches the end of the paste for reversed Base64 encoding structure.
-        hexmatch2 = re.search(r'[2-9A-F]{200,}', paste_data) #Regex for Hexadecimal encoding.
-        phpmatch = re.search(r'\A(<\?php)', paste_data) #Searches the start of a paste for php structure.
-        imgmatch = re.search(r'\A(data:image)', paste_data) #Searches the start of a paste for data:image structure.
-        if ((base64match or stringmatch) and int(paste_size) > 40000) and paste_lang == "text" and coll_pastemetadata.find_one({'key':paste['key']}) is None:
-            if (binarymatch and paste_data.isnumeric()):
-                filename = save_path_binary + paste['key']
-                encodingtype = 'binary'
-                save_paste(filename, paste_data)
-                metadata = metadatasave(paste, encodingtype)
-                coll_pastemetadata.insert_one(metadata)
-                hipchatpost()
-            elif (base64sort or base64reversesort):
-                filename = save_path_base64 + paste['key']
-                encodingtype = 'base64'
-                save_paste(filename, paste_data)
-                metadata = metadatasave(paste, encodingtype) 
-                coll_pastemetadata.insert_one(metadata)
-                hipchatpost()
-            elif (hexmatch or hexmatch2):
-                filename = save_path_hex + paste['key']
-                encodingtype = 'hexadecimal'
-                save_paste(filename, paste_data)
-                metadata = metadatasave(paste, encodingtype)
-                coll_pastemetadata.insert_one(metadata)
-                hipchatpost()
-            elif phpmatch:
-                filename = save_path_php + paste['key']
-                encodingtype = 'php'
-                save_paste(filename, paste_data)
-                metadata = metadatasave(paste, encodingtype)
-                coll_pastemetadata.insert_one(metadata)
-                hipchatpost()
-            elif imgmatch:
-                filename = save_path_img + paste['key']
-                encodingtype = 'img'
-                save_paste(filename, paste_data)
-                metadata = metadatasave(paste, encodingtype)
-                coll_pastemetadata.insert_one(metadata)
-                hipchatpost()
-            else:
-                filename = save_path + paste['key']
-                encodingtype = 'other'
-                save_paste(filename, data)
-                metadata = metadatasave(paste, encodingtype)
-                coll_pastemetadata.insert_one(metadata)
-                hipchatpost()
-            hits += 1
-        print("\nHits: {0}".format(hits))
-    print("Waiting...\n\n")
-    time.sleep(sleep_time)
+    while True:
+        clock = int(time.strftime('%M', time.localtime()))
+        if clock == 5:
+            trendscraper()
+        else:
+            print("Waiting for new trends.")
+        hits = 0
+        recent_items = http_get('http://pastebin.com/api_scraping.php', params={'limit': result_limit}).json()
+        for i, paste in enumerate(recent_items):
+            paste_data = http_get(paste['scrape_url']).text
+            paste_lang = paste['syntax']
+            paste_size = paste['size']
+            paste_url = paste['full_url']
+            print('\rScraping: {0} / {1}'.format(i + 1, result_limit))
+            hexmatch = re.search(r'(\\x\w\w){100,}', paste_data) #Regex for hex formatted as "\\xDC", "\\x02", "\\xC4"
+            stringmatch = re.search(r'(A){20}', paste_data) #Searching for 10 'A's in a row.
+            base64match = re.search(r'\w{200,}', paste_data) #Searching for 200 characters in a row to get non-words.
+            base64sort = re.search(r'\A(TV(oA|pB|pQ|qQ|qA|ro|pA))', paste_data) #Searches the start of the paste for Base64 encoding structure.
+            binarymatch = re.search(r'(0|1){200,}', paste_data) #Searches for 200 0's or 1's in a row.
+            base64reversesort = re.search(r'\Z(AAAMAAQqVT)', paste_data) #Searches the end of the paste for reversed Base64 encoding structure.
+            hexmatch2 = re.search(r'[2-9A-F]{200,}', paste_data) #Regex for Hexadecimal encoding.
+            phpmatch = re.search(r'\A(<\?php)', paste_data) #Searches the start of a paste for php structure.
+            imgmatch = re.search(r'\A(data:image)', paste_data) #Searches the start of a paste for data:image structure.
+            if ((base64match or stringmatch) and int(paste_size) > 40000) and paste_lang == "text" and coll_pastemetadata.find_one({'key':paste['key']}) is None:
+                if (binarymatch and paste_data.isnumeric()):
+                    filename = save_path_binary + paste['key']
+                    encodingtype = 'binary'
+                    save_paste(filename, paste_data)
+                    metadata = metadatasave(paste, encodingtype)
+                    coll_pastemetadata.insert_one(metadata)
+                    hipchatpost()
+                elif (base64sort or base64reversesort):
+                    filename = save_path_base64 + paste['key']
+                    encodingtype = 'base64'
+                    save_paste(filename, paste_data)
+                    metadata = metadatasave(paste, encodingtype) 
+                    coll_pastemetadata.insert_one(metadata)
+                    hipchatpost()
+                elif (hexmatch or hexmatch2):
+                    filename = save_path_hex + paste['key']
+                    encodingtype = 'hexadecimal'
+                    save_paste(filename, paste_data)
+                    metadata = metadatasave(paste, encodingtype)
+                    coll_pastemetadata.insert_one(metadata)
+                    hipchatpost()
+                elif phpmatch:
+                    filename = save_path_php + paste['key']
+                    encodingtype = 'php'
+                    save_paste(filename, paste_data)
+                    metadata = metadatasave(paste, encodingtype)
+                    coll_pastemetadata.insert_one(metadata)
+                    hipchatpost()
+                elif imgmatch:
+                    filename = save_path_img + paste['key']
+                    encodingtype = 'img'
+                    save_paste(filename, paste_data)
+                    metadata = metadatasave(paste, encodingtype)
+                    coll_pastemetadata.insert_one(metadata)
+                    hipchatpost()
+                else:
+                    filename = save_path + paste['key']
+                    encodingtype = 'other'
+                    save_paste(filename, data)
+                    metadata = metadatasave(paste, encodingtype)
+                    coll_pastemetadata.insert_one(metadata)
+                    hipchatpost()
+                hits += 1
+            print("\nHits: {0}".format(hits))
+        print("Waiting...\n\n")
+        time.sleep(sleep_time)
 if __name__ == "__main__":
     while True:
         scrapebin()
