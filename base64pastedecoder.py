86510de9b6305dd441a5f38591ddc3c81d830ea5
Patrick Colford <kaoticrequiem@gmail.com>
2017-10-28 16:47:41 -0700

    Fixed decoders' paths to relative. Updated README.

--- base64pastedecoder.py
+++ base64pastedecoder.py
@@ -1,17 +1,17 @@
 import base64, os, requests
 
-base64list = []
-decoded_save_path = '/home/ubuntu/patrick/decodedexes/'
+base64directory = os.getcwd() + '/pastes/base64pastes/' #relative path of Base64 pastes.
+save_path = os.getcwd() + '/decodedexes/' #relative path of stored executables.
 
 def writefile(filenm, stuff):
-   writefile = open(filenm,'w')
-   writefile.write(stuff)
-   writefile.close()
+    writefile = open(filenm,'w')
+    writefile.write(stuff)
+    writefile.close()
 
-for filename in os.listdir('/home/ubuntu/patrick/pastes/base64pastes/'): # absolute path of saved base64 sorted pastes
-    sorted_path = os.path.join('/home/ubuntu/patrick/pastes/base64pastes/', filename)
-    outputfile = decoded_save_path + filename
-    with open(sorted_path, 'r') as f:
+for filename in os.listdir(base64directory): 
+    paste = os.path.join(base64directory, filename)
+    outputfile = save_path + filename
+    with open(paste, 'r') as f:
         paste_data = f.read()
         missing_padding = len(paste_data) % 4
         if missing_padding != 0:
@@ -19,7 +19,7 @@ for filename in os.listdir('/home/ubuntu/patrick/pastes/base64pastes/'): # absol
         try:
             decoded_paste = base64.b64decode(paste_data)
             writefile(outputfile, decoded_paste) # write pe32exe
-            os.remove(sorted_path)
+            os.remove(paste)
         except:
             continue
         f.close()
--- binarypastedecoder.py
+++ binarypastedecoder.py
@@ -1,26 +1,24 @@
 import binascii, os, requests
 
-base64list = []
-save_path = '/home/ubuntu/patrick/decodedexes/'
-length = 8
+binarydirectory = os.getcwd() + '/pastes/binarypastes/' #relative path of binary pastes.
+save_path = os.getcwd() + '/decodedexes/' #relative path of stored executables.
 
-def writefile(path, binary):
-    writefile = open(path,'wb')
-    writefile.write(binary)
+def writefile(filenm, stuff):
+    writefile = open(filenm,'w')
+    writefile.write(stuff)
     writefile.close()
     
-for filename in os.listdir('/home/ubuntu/patrick/pastes/binarypastes/'):
-    sorted_path = os.path.join('/home/ubuntu/patrick/pastes/binarypastes/', filename)
+for filename in os.listdir(binarydirectory):
+    paste = os.path.join(binarydirectory, filename)
     length = 8
     outputfile = save_path + filename
-    with open(sorted_path, 'r') as f:
+    with open(paste, 'r') as f:
         paste_data = f.read()
         paste_data_length = [paste_data[i:i+length] for i in range(0,len(paste_data),length)]
         try:
             decoded_paste = ''.join([chr(int(c,base=2)) for c in paste_data_length])
             writefile(outputfile, decoded_paste)
-            os.remove(sorted_path)
+            os.remove(paste)
         except:
             continue
         f.close()
-
