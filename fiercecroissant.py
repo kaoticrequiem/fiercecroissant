cf82452325215683dd8a99a7ea5c3253190b6f50
Patrick Colford <kaoticrequiem@gmail.com>
2017-12-01 13:23:22 -0800

    Changed initial filter to look for broken up base64 pastes.

--- fiercecroissant.py
+++ fiercecroissant.py
@@ -92,7 +92,7 @@ def scrapebin():
             hexmatch2 = re.search(r'[2-9A-F]{200,}', paste_data) #Regex for Hexadecimal encoding.
             phpmatch = re.search(r'\A(<\?php)', paste_data) #Searches the start of a paste for php structure.
             imgmatch = re.search(r'\A(data:image)', paste_data) #Searches the start of a paste for data:image structure.
-            if ((nonwordmatch or stringmatch) and int(paste_size) > 40000) and paste_lang == "text" and coll_pastemetadata.find_one({'key':paste['key']}) is None:
+            if (((nonwordmatch or stringmatch) or (stringmatch_76 and (base64sort or base64reversesort))) and int(paste_size) > 40000) and paste_lang == "text" and coll_pastemetadata.find_one({'key':paste['key']}) is None:
                 if (binarysort and paste_data.isnumeric()):
                     filename = save_path_binary + paste['key']
                     encodingtype = 'binary'
