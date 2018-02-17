e62b7b1f0d32a71f5c9f3c89a827a391dd46f2dd
Patrick Colford <kaoticrequiem@gmail.com>
2018-02-16 16:33:15 -0800

    Update binarypastedecoder.py
    Removed binascii, which is not used.

--- binarypastedecoder.py
+++ binarypastedecoder.py
@@ -1,4 +1,4 @@
-import binascii, os
+import os
 
 binarydirectory = os.getcwd() + '/pastes/binarypastes/' #relative path of binary pastes.
 save_path = os.getcwd() + '/decodedexes/' #relative path of stored executables.
